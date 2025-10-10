6f744a71cfdd0d88b704d82c8266991ac05fff3c
Fernando Levi Marquardt <fernando.marquardt@king.com>
2025-10-10 16:08:32 +0100

    Add delay and print logs

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -3,12 +3,15 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static java.lang.Thread.sleep;
+
 @SpringBootTest
 class DemoApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void contextLoads() throws InterruptedException {
 		System.out.println("Test executed successfully");
+		sleep(3000); // Simulate a delay of 2 seconds
 	}
 
 }
